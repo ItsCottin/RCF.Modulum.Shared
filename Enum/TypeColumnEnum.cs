0bbff1dc5ad4f6479ff800cfe2c059b721dfed21
Rodrigo Cotting <cottingfontes@hotmail.com>
2025-04-26 03:07:53 -0300

    Alteração no Enum de tipo de coluna

---
+++ Enum/TypeColumnEnum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace modulum.Shared.Enum
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum TypeColumnEnum
+    {
+        [Display(Name = "Texto")]
+        VARCHAR,
+
+        [Display(Name = "Número")]
+        INT,
+
+        [Display(Name = "Data")]
+        DATE,
+
+        [Display(Name = "'Sim' ou 'Não'")]
+        BOOLEAN
+    }
+}
